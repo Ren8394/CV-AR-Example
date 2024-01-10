95ec498db3346ae2d1c1d62952c8aca98cafaaaf
Ren <renchen8394@gmail.com>
2024-01-10 10:54:44 +0800

    epochs to 25

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     # hyperparameters
     batch_size = 32
     lr = 1e-4
-    epochs = 10
+    epochs = 25
 
     # optimizer
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
